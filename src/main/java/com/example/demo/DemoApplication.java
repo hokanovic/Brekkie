15c1094c84e90b102ff7b57982244317f4f5e976
Daniel Lecoq <daninstockings@gmail.com>
2018-04-24 18:02:15 +0200

    Try again

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.DBConnectionTest.SQLQuery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello Logs!");
+		SQLQuery test = new SQLQuery();
+		String productName = test.getProductName();
+		System.out.println(productName);
 	}
 }
