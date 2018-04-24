5f350ba4ee7cc4c167945aee9246fe3233d1c6ba
Rasmus Ottosson <rasot112@gmail.com>
2018-04-24 09:49:01 +0200

    Created function to add products to database

--- brekkie/src/main/java/com/example/demo/repository/JDBCRepository.java
+++ brekkie/src/main/java/com/example/demo/repository/JDBCRepository.java
@@ -8,7 +8,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JDBCRepository implements ShopRepository{
+public class JDBCRepository implements ShopRepository {
     private DataSource dataSource;
     private List<Order> orderList = new ArrayList<>();
     private List<Product> productList = new ArrayList<>();
@@ -42,18 +42,18 @@ public class JDBCRepository implements ShopRepository{
 
     @Override
     public void addOrder(int id, int customerId, String orderDate, String paymentOption, String marking,
-                  String experationDate, String deliveryDate, String deliveryTime) {
+                         String experationDate, String deliveryDate, String deliveryTime) {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement ps = conn.prepareStatement("INSERT INTO order(id, customerID, orderDate, paymentOption," +
-                     "marking, experationDate, deliveryDate, deliveryTime) VALUES (?,?,?,?,?,?,?,?) ", new String[] {"id"})) {
+                     "marking, experationDate, deliveryDate, deliveryTime) VALUES (?,?,?,?,?,?,?,?) ", new String[]{"id"})) {
             ps.setInt(1, id);
             ps.setInt(2, customerId);
             ps.setString(3, orderDate);
-            ps.setString(4,paymentOption);
-            ps.setString(5,marking);
-            ps.setString(6,experationDate);
-            ps.setString(7,deliveryDate);
-            ps.setString(8,deliveryTime);
+            ps.setString(4, paymentOption);
+            ps.setString(5, marking);
+            ps.setString(6, experationDate);
+            ps.setString(7, deliveryDate);
+            ps.setString(8, deliveryTime);
             ps.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -62,13 +62,22 @@ public class JDBCRepository implements ShopRepository{
     }
 
     @Override
-     public void addProduct(int id, String name, int productCategori_id) {
-
+    public void addProduct(int id, String name, int productCategory_id) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement("INSERT INTO product(id, name, productCategory_id) " +
+                     "VALUES (?,?,?) ", new String[]{"id"})) {
+            ps.setInt(1, id);
+            ps.setString(2, name);
+            ps.setInt(3, productCategory_id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Order rsOrder(ResultSet rs) throws SQLException {
         return new Order(rs.getInt("id"), rs.getInt("customerId"), rs.getString("orderDate"),
-                rs.getString("paymentOption"), rs.getString("marking"),rs.getString("experationDate"),
+                rs.getString("paymentOption"), rs.getString("marking"), rs.getString("experationDate"),
                 rs.getString("deliveryDate"), rs.getString("deliveryTime"));
     }
 
